63aa4c37c124353b68db73263a95a5a674c4c32a
Marcel Grünauer <marcel@cpan.org>
2010-03-27 14:38:28 +0000

    Import of MARCEL/Regexp-Common-AT-Profanity-1.100860 from CPAN.
    gitpan-cpan-distribution: Regexp-Common-AT-Profanity
    gitpan-cpan-version:      1.100860
    gitpan-cpan-path:         MARCEL/Regexp-Common-AT-Profanity-1.100860.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Regexp/Common/AT/Profanity.pm
+++ lib/Regexp/Common/AT/Profanity.pm
@@ -1,168 +1,153 @@
-package Regexp::Common::AT::Profanity;
-
+use 5.008;
 use strict;
 use warnings;
+
+package Regexp::Common::AT::Profanity;
+our $VERSION = '1.100860';
+# ABSTRACT: Provide regexes for profanity in Austrian German
 use Regexp::Common qw /pattern clean no_defaults/;
 use Regexp::Assemble;
 use HTML::Entities;
 
-
-our $VERSION = '0.02';
-
-
 # rot13 in vim: g?{motion}
-
 my @nouns = qw(
-    (nefpu|bnfpu)(tr?fvpug|xrxf|yrpxre|ybpu|jnemra?)?
-    (qerpxf|fpurv&fmyvt;)?gfpuhfpu(ra)?(fnh)?
-    nygre\f+fnpx
-    nezyrhpugre
-    onaxreg
-    onfgneq
-    orvqr?y(cenpxre)?
-    ovgpu
-    oy&bhzy;qznaa
-    ohzfrerv
-    qnezsybevfg
-    qrcc
-    qvyyb
-    qbyz
-    qerpxfnh
-    qh\f+bcsre
-    qhzzrewna
-    qhzcsonpxr
-    srggr\f+fnh
-    srggfnpx
-    srggfnh
-    srgmr?afpu&nhzy;qry
-    svpxr(a|e(rv)?)
-    shpx
-    shpxvat
-    shg
-    trfvpugffpunoenpxr
-    trfvpugfibgmr
-    uveav
-    ubuyxbcs
-    uhaqfsbgg
-    uher
-    uher?a(orvqr?y|xvaq|fbua)
-    uhererv
-    vqvbg
-    vue\f+bcsref?
-    whqraoratry
-    whqrafnh
-    xnanxra?
-    xanyypunetr
-    xanyyxbcs
-    xbgmserffr
-    y&hhzy;zzry
-    yrpx\f+zvpu
-    zvfgfg&hhzy;px
-    avttre
-    cvffre
-    cengreuher?
-    chqrenag
-    chqrerv
-    enhfpuxvaq
-    fnpxenggr
-    fnhwhqr?
-    fpujnamyhgfpure
-    fpujhpugry
-    fpujhyr\f+fnh
-    freivreshg
-    fcnpxb
-    fcnfgv?
-    fg&hhzy;px\f+qerpx
-    gnfpuraovyyneq
-    gebggry
-    hathfgr?y
-    ibyyvqvbg
-    ibyyxbssre
-    ibgmr
-    jv(kk?|puf)(re(rv)?|ibeyntr)
+  (nefpu|bnfpu)(tr?fvpug|xrxf|yrpxre|ybpu|jnemra?)?
+  (qerpxf|fpurv&fmyvt;)?gfpuhfpu(ra)?(fnh)?
+  nygre\f+fnpx
+  nezyrhpugre
+  onaxreg
+  onfgneq
+  orvqr?y(cenpxre)?
+  ovgpu
+  oy&bhzy;qznaa
+  ohzfrerv
+  qnezsybevfg
+  qrcc
+  qvyyb
+  qbyz
+  qerpxfnh
+  qh\f+bcsre
+  qhzzrewna
+  qhzcsonpxr
+  srggr\f+fnh
+  srggfnpx
+  srggfnh
+  srgmr?afpu&nhzy;qry
+  svpxr(a|e(rv)?)
+  shpx
+  shpxvat
+  shg
+  trfvpugffpunoenpxr
+  trfvpugfibgmr
+  uveav
+  ubuyxbcs
+  uhaqfsbgg
+  uher
+  uher?a(orvqr?y|xvaq|fbua)
+  uhererv
+  vqvbg
+  vue\f+bcsref?
+  whqraoratry
+  whqrafnh
+  xnanxra?
+  xanyypunetr
+  xanyyxbcs
+  xbgmserffr
+  y&hhzy;zzry
+  yrpx\f+zvpu
+  zvfgfg&hhzy;px
+  avttre
+  cvffre
+  cengreuher?
+  chqrenag
+  chqrerv
+  enhfpuxvaq
+  fnpxenggr
+  fnhwhqr?
+  fpujnamyhgfpure
+  fpujhpugry
+  fpujhyr\f+fnh
+  freivreshg
+  fcnpxb
+  fcnfgv?
+  fg&hhzy;px\f+qerpx
+  gnfpuraovyyneq
+  gebggry
+  hathfgr?y
+  ibyyvqvbg
+  ibyyxbssre
+  ibgmr
+  jv(kk?|puf)(re(rv)?|ibeyntr)
 );
-
-my $adj_dekl = "(e[mnrs]?)?";
+my $adj_dekl   = "(e[mnrs]?)?";
 my @adjectives = qw(
-    (or|ire)(fpuvffra|xnpxg)
-    (ibyy|na)tr(fpuvffra|xnpxg)
-    oy&bhzy;q
-    oynq
-    oehamryaq
-    qrccreg
-    qbbs
-    svfpuryaq
-    cvffra
-    fpurv(&fmyvt;|ff)
-    ireqnzzg
-    iresvpxg
-    iresyhpug
-    ireuheg
-    gebggryvt
-    iregebggryg
+  (or|ire)(fpuvffra|xnpxg)
+  (ibyy|na)tr(fpuvffra|xnpxg)
+  oy&bhzy;q
+  oynq
+  oehamryaq
+  qrccreg
+  qbbs
+  svfpuryaq
+  cvffra
+  fpurv(&fmyvt;|ff)
+  ireqnzzg
+  iresvpxg
+  iresyhpug
+  ireuheg
+  gebggryvt
+  iregebggryg
 );
-
-
 my $verb_dekl = '';
-my @verbs = qw(
-    nofcevgmra
-    notrfcevgmg
-    ohzfra
-    oehamry?a
-    svfpurya
-    trsvpxg
-    xvssra
-    urehzuhera
-    urehzfpujhpugrya
-    chqrea
-    fnhsra
+my @verbs     = qw(
+  nofcevgmra
+  notrfcevgmg
+  ohzfra
+  oehamry?a
+  svfpurya
+  trsvpxg
+  xvssra
+  urehzuhera
+  urehzfpujhpugrya
+  chqrea
+  fnhsra
 );
-
 tr/A-Za-z/N-ZA-Mn-za-m/ for @nouns, @verbs, @adjectives;
-
 my @profanity = @nouns;
 
 # verbs ending in -en or -ern can be made into adjectives by adding -d
-
-push @profanity =>
-    map { "$_$adj_dekl"  }
-    @adjectives,
-        map  { $_ . 'd' }
-        grep { /er?n$/ }
-        @verbs;
-
-push @profanity =>
-    map { "$_$verb_dekl" }
-    @verbs;
-
+push @profanity => map { "$_$adj_dekl" } @adjectives,
+  map  { $_ . 'd' }
+  grep { /er?n$/ } @verbs;
+push @profanity => map { "$_$verb_dekl" } @verbs;
 my $assembler = Regexp::Assemble->new(flags => 'i');
 for (@profanity) {
     decode_entities($_);
     $assembler->add($_);
 }
 
-
 # the '\x{'.'%s}' kludge is so it doesn't look like a template start tag
-
 (my $profanity = $assembler->re) =~ s/(.)/
     ord($1) > 127
         ? sprintf('\x{'.'%s}', unpack("H*", pack("n", ord($1))))
         : $1
     /ge;
-
-pattern name   => [ qw(at profanity) ],
-        create => '(?:\b(?k:' . $profanity . ')\b)';
-
+pattern
+  name   => [qw(at profanity)],
+  create => '(?:\b(?k:' . $profanity . ')\b)';
 1;
 
 
 __END__
-
-
+=pod
 
 =head1 NAME
 
-Regexp::Common::AT::Profanity - provide regexes for profanity in Austrian German
+Regexp::Common::AT::Profanity - Provide regexes for profanity in Austrian German
+
+=head1 VERSION
+
+version 1.100860
 
 =head1 SYNOPSIS
 
@@ -202,44 +187,39 @@ captures the entire word
 
 =back
 
-=head1 TAGS
+=head1 INSTALLATION
 
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<regexpcommonatprofanity> tag.
-
-=head1 VERSION 
-                   
-This document describes version 0.02 of L<Regexp::Common::AT::Profanity>.
+See perlmodinstall for information and options on installing Perl modules.
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
-Please report any bugs or feature requests to
-C<<bug-regexp-common-at-profanity@rt.cpan.org>>, or through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Regexp-Common-AT-Profanity>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Regexp-Common-AT-Profanity/>.
+
+The development version lives at
+L<http://github.com/hanekomu/Regexp-Common-AT-Profanity/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
 =head1 AUTHOR
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+This software is copyright (c) 2007 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
--- t/01_misc.t
+++ t/01_misc.t
@@ -1,5 +1,4 @@
 #!/usr/bin/env perl
-
 use warnings;
 use strict;
 use Regexp::Common 'AT::Profanity';
@@ -7,7 +6,6 @@ use HTML::Entities;
 use Test::More;
 
 # rot13 in vim: g?{motion}
-
 my @like = (
     'Qh Uherafbua, Qh!',
     'Nefpuybpu',
@@ -17,26 +15,17 @@ my @like = (
     'rva xvssraqrf xvaq',
     'qrz chqreaqrz znaa'
 );
-
-my @unlike = (
-    'Jawoll, mein Herr',
-    'Es ist so sch&ouml;n',
-    'Ihr Florist',
-    'Iiiinsel!',
-);
-
+my @unlike =
+  ('Jawoll, mein Herr', 'Es ist so sch&ouml;n', 'Ihr Florist', 'Iiiinsel!',);
 plan tests => @like + @unlike;
-
 for my $like (@like) {
     my $got = $like;
     $got =~ tr/A-Za-z/N-ZA-Mn-za-m/;
     decode_entities($got);
     like($got, qr/$RE{at}{profanity}/i, "match: $like");
 }
-
 for my $unlike (@unlike) {
     my $got = $unlike;
     decode_entities($got);
     unlike($got, qr/$RE{at}{profanity}/i, "no match: $unlike");
 }
-
